81e7843e59e28d65db30a58f26a3ef07e6d23e32
Muhammad Zakir <hi@zakir.id>
2023-10-08 20:42:29 +0700

    Revert from using 'gh-pages' library

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,8 @@ const config = {
 	},
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: undefined,
 			precompress: false,
 			strict: false
