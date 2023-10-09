f1e4caed15f279f307744b8b1dc94817e30751c1
Muhammad Zakir <hi@zakir.id>
2023-10-09 20:59:15 +0700

    Patch the page to show coming soon and add the meta information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<svelte:head>
+	<title>Homepage - Muhammad Zakir</title>
+	<meta name="description" content="Homepage of Muhammad Zakir's personal website" />
+</svelte:head>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Skeleton.</h2>
-		<!-- Animated Logo -->
+		<h2 class="h2">Coming Soon</h2>
 		<figure>
 			<section class="img-bg" />
 			<svg
@@ -17,22 +19,6 @@
 				/>
 			</svg>
 		</figure>
-		<!-- / -->
-		<div class="flex justify-center space-x-2">
-			<a
-				class="btn variant-filled"
-				href="https://skeleton.dev/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				Launch Documentation
-			</a>
-		</div>
-		<div class="space-y-2">
-			<p>Try editing the following:</p>
-			<p><code class="code">/src/routes/+layout.svelte</code></p>
-			<p><code class="code">/src/routes/+page.svelte</code></p>
-		</div>
 	</div>
 </div>
 
