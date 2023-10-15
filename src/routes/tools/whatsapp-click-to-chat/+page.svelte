458c6f869cf4060ce0a2ab686b2e11f18fe3a611
Muhammad Zakir <hi@zakir.id>
2023-10-15 17:38:37 +0700

    Add parser for basic international phone number format

--- src/routes/tools/whatsapp-click-to-chat/+page.svelte
+++ src/routes/tools/whatsapp-click-to-chat/+page.svelte
@@ -1,5 +1,13 @@
 <script>
-  let whatsAppNumber = null;
+  let whatsAppNumber = '';
+
+  function parseWhatsAppNumber() {
+    whatsAppNumber = whatsAppNumber.replaceAll(/[^0-9.]/g, '')
+
+    if (whatsAppNumber.length >= 15) {
+      whatsAppNumber = whatsAppNumber.substring(0,15);
+    }
+  }
 </script>
 
 <svelte:head>
@@ -9,8 +17,8 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
   <div class="space-y-10 text-center flex flex-col items-center">
-    <input class="input" title="Input WhatsApp number" type="tel" placeholder="+62123456789" bind:value={whatsAppNumber}/>
-    {#if whatsAppNumber}
+    <input class="input" title="Input WhatsApp number" type="tel" placeholder="628123456789" bind:value={whatsAppNumber} on:input={parseWhatsAppNumber}/>
+    {#if whatsAppNumber.length >= 7}
       <a href="https://wa.me/{whatsAppNumber}" class="btn bg-primary-500">
         <span class="flex-auto">Start chat</span>
       </a>
