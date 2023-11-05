676d27f1b5b66141df4cc59081e68c464c6f5cfb
Muhammad Zakir <hi@zakir.id>
2023-11-05 17:10:41 +0700

    Add the new page's link and patch the AppBar's width to fit the content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<Drawer width="w-[20vw]">
+<Drawer width="w-fit">
 	<AppRail>
 		<svelte:fragment slot="lead">
 			<AppRailAnchor href="/" selected={$page.url.pathname === '/'} on:click={() => (drawerStore.close())}>
@@ -37,6 +37,16 @@
 					</span>
 				</svelte:fragment>
 				<span>WhatsApp Click to Chat</span>
+			</AppRailAnchor>
+			<AppRailAnchor href="/frequently-asked-questions" selected={$page.url.pathname === '/frequently-asked-questions'} on:click={() => (drawerStore.close())}>
+				<svelte:fragment slot="lead">
+					<span class="badge">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+						  <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+						</svg>
+					</span>
+				</svelte:fragment>
+				<span>Frequently Asked Questions</span>
 			</AppRailAnchor>
 		</svelte:fragment>
 	</AppRail>
